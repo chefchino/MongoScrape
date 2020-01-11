6023392429964f640c6829f8b0a1e8f001502301
Ciprien Bossut <chefchino@live.com>
2020-01-11 17:34:12 -0500

    still working on layout

--- server.js
+++ server.js
@@ -1,5 +1,45 @@
 var axios = require("axios");
 var cheerio = require("cheerio");
 var express = require("express");
-var exhbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
+
+
+var app = express();
+var PORT = process.env.PORT || 3001;
+app.use(express.static("public"));
+app.engine(
+    "handlebars",
+    exphbs({
+      defaultLayout: "main"
+    })
+  );
+  app.set("view engine", "handlebars");
+  
+var databaseUrl = "news";
+var collections = ["articles"];
+
+// var db = mongojs(databaseUrl, collections);
+
+// db.on("error", function(error) {
+//     console.log("Database Error:" , error);
+// });
+
+// app.get("/", function(req, res) {
+//     res.send("I'm IN");
+// });
+
+// app.get("/all", function(req, res) {
+//     db.articles.find({}, function(error, found) {
+//         if (error) {
+//             console.log(error);
+//         }
+//         else {
+//             res.json(found);
+//         }
+//     });
+// });
+
+app.listen(PORT, function() {
+    console.log("App running on PORT 3000+1!!");
+});
